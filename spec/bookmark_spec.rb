56bd4163acd7743b42c54660acdff30d3fa7ed96
anitaRead <areadman91@gmail.com>
2021-06-22 17:44:41 +0100

    Refactor spec files

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,7 +2,7 @@ require 'bookmark'
 
 describe Bookmark do
     it 'returns a list of bookmarks' do
-      p Bookmark.all
-      expect(Bookmark.all).to include("http://www.makersacademy.com", "http://www.twitter.com", "http://www.google.com", "http://www.destroyallsoftware.com")
+      bookmark_list = Bookmark.all
+      expect(bookmark_list).to include("http://www.makersacademy.com", "http://www.twitter.com", "http://www.google.com", "http://www.destroyallsoftware.com")
     end
 end
