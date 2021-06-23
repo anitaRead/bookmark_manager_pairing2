8d7d7bbfffa8b618432da136b04939db25631493
anitaRead <areadman91@gmail.com>
2021-06-23 15:23:25 +0100

    Refactor view_bookmarks test to use create method

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -7,11 +7,10 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'user can view bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
 
     visit('/bookmarks')
 
