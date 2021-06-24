88fbb2fb539e88feaf277a5cf42940a6d8d28df7
anitaRead <areadman91@gmail.com>
2021-06-24 16:57:46 +0100

    Update Bookmark.create to protect against SQL injection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -22,18 +22,17 @@ class Bookmark
     end
   end
 
-
-
-  def self.create(title:, url:)
+  def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+     connection = PG.connect(dbname: 'bookmark_manager_test')
     else
-      connection = PG.connect(dbname: 'bookmark_manager')
+     connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, title, url")
+    result = connection.exec_params(
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
-    
-
+   
 end
